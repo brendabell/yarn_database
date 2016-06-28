2a35ea0761af275706ecb29b5e645a52b30c9318
Brenda <b311b-github@theotherbell.com>
2016-06-27 20:42:15 -0400

    Update yarn.sql

--- yarn.sql
+++ yarn.sql
@@ -2,6 +2,10 @@ CREATE TABLE `yarn_yarn` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `manufacturer` varchar(128) NOT NULL,
+  `rows_per_4cm` decimal(2,1) NOT NULL,
+  `stitches_per_10cm` decimal(2,1) NOT NULL,
+  `needle_size` varchar(128),
+  `hook_size` varchar(128),
   PRIMARY KEY (`id`),
   UNIQUE KEY `yarn_name` (`name`),
   KEY `yarn_manufacturer` (`manufacturer`)
@@ -24,8 +28,8 @@ CREATE TABLE `yarn_swatch` (
   `machine` enum('White 1602','Singer 2200','Brother KH860','Studio SK155','Silver Reed SK860') NOT NULL,
   `stitch_size` decimal(2,1) NOT NULL,
   `mast_tension` int(11) NOT NULL,
-  `rows_per_4cm` decimal(2,1) NOT NULL,
-  `stitches_per_10cm` varchar(45) NOT NULL,
+  `rows_per_10cm` decimal(2,1) NOT NULL,
+  `stitches_per_10cm` decimal(2,1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `swatch_yarn` (`yarn_id`),
   CONSTRAINT `swatch_yarn` FOREIGN KEY (`yarn_id`) REFERENCES `yarn_yarn` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
